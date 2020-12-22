92bc7ab43fb24eb0756dadd9368332b11f7ac002
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-22 16:05:50 +0200

    Add missed actions

--- cypress/integration/pageObjects/forgetCaseLog.js
+++ cypress/integration/pageObjects/forgetCaseLog.js
@@ -1,34 +1,18 @@
 class forgetPassword {
 
-    signInButton() {
-
-        return cy.get('[data-testid="signin_button"]')
-
-    }
-
     forgetPasswordButton() {
-
         return cy.get('[data-testid="forget_password_link"]')
-
     }
 
-    forgetEmail() {
-
+    Email() {
         return cy.get('[data-testid="forget_password_email"]')
-
     }
 
     resetButton() {
-
         return cy.get('[data-testid="forget_password_button"]')
     }
-
    okButton() {
-
         return cy.get('.alert-container > .mat-focus-indicator')
-    }
-
-    
-
+    }   
 }
 export default forgetPassword 
