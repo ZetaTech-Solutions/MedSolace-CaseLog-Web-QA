2f8eac8b0c650d9c76c5ee6eee9abafb9e12a991
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-22 15:41:45 +0200

    [MCL-180] Verify forget password process

---
+++ cypress/integration/caseLog/Forget Password/TC01_forgetPassword.spec.js
@@ -0,0 +1,20 @@
+import forgetPassword from '../../pageObjectsCaseLog/forgetCaseLog'
+
+describe('Forget Password', function () {
+    const forgetpassword = new forgetPassword()
+    it('Forget Password', function () {
+        cy.server()
+        cy.route('POST', '/api/v1/user/ForgotPassword').as('forgetPassword')
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.visit(url.WebsiteUrl)
+
+            cy.fixture('forgetDetailsCaseLog').then((user) => {
+                forgetpassword.forgetPasswordButton().click()
+                cy.url().should('include', 'reset-password')
+                forgetpassword.Email().type(user.forgetEmail)
+                forgetpassword.resetButton().should('be.visible').click()
+            })
+            cy.wait('@forgetPassword').its('status').should('eq', 200)
+        })
+    })
+})
