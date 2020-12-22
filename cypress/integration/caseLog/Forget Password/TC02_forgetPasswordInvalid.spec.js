577f3c2a7f81646ee0a43d59bcd4daf8cd10aaf0
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-22 16:05:17 +0200

    [MCL-263] Verify forget password process with invalid input

---
+++ cypress/integration/caseLog/Forget Password/TC02_forgetPasswordInvalid.spec.js
@@ -0,0 +1,62 @@
+import forgetPassword from '../pageObjectsCaseLog/forgetCaseLog'
+
+describe('Forget Password invalid', function () {
+    const forgetpassword = new forgetPassword()
+
+    it('Open Forget Password dialog', function () {
+            cy.fixture('WebsiteUrl').then((url)=>{
+            cy.visit(url.WebsiteUrl)
+            forgetpassword.forgetPasswordButton().should('be.visible').click()
+            cy.contains("We need your registered e-mail address to send you a reset password link.").should('be.visible')
+
+            it('Forget Password without enter email', function () {
+                forgetpassword.resetButton().should('be.visible').click()
+                cy.contains('This feild is required').should('be.visible')
+            })
+
+
+            it('Forget Password with invalid email', function () {
+
+                cy.fixture('forgetDetailsCaseLog').then((user) => {
+                    forgetpassword.forgetEmail().type(user.invalidForgetEmail)
+                    forgetpassword.resetButton().should('be.visible').click()
+                    cy.contains('Please enter a valid email').should('be.visible')
+                })
+            })
+
+            it('Forget Password with un used email', function () {
+                cy.server()
+                cy.route('POST', '/api/v1/user/ForgotPassword').as('forgetPassword')
+                cy.fixture('forgetDetailsCaseLog').then((user) => {
+                    forgetpassword.forgetEmail().clear().type(user.unUsedEmail)
+                    forgetpassword.resetButton().should('be.visible').click()
+                })
+                cy.wait('@forgetPassword').its('status').should('eq', 200)
+            })
+
+            it('Forget Password with un verified email', function () {
+                cy.server()
+                cy.route('POST', '/api/v1/user/ForgotPassword').as('forgetPassword')
+                forgetpassword.forgetPasswordButton().should('be.visible').click()
+                cy.fixture('forgetDetailsCaseLog').then((user) => {
+                    forgetpassword.forgetEmail().clear().type(user.unActiveEmail)
+                    forgetpassword.resetButton().should('be.visible').click()
+                })
+                cy.wait('@forgetPassword').its('status').should('eq', 200)
+            })
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
