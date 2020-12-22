59e38a705b21d26c3ce7bafdbb7a33026a8df8cb
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-22 15:05:44 +0200

    [MCL-192] Verify from the "Re-send the verification email" button

---
+++ cypress/integration/caseLog/SignUp/TC03_Re-send_verification_code.js
@@ -0,0 +1,35 @@
+import signUp from '../../pageObjects/signupCaseLog'
+
+describe('Sign Up', function () {
+    const signup = new signUp()
+    it('Sign Up', function () {
+        cy.server()
+        cy.route({
+            method: 'POST',
+            url: '/api/v1/user/ValidateEmail',
+        }).as('apiCheck')
+
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.visit(url.WebsiteUrl+"signup")
+
+            cy.fixture('userSignUpDetailsCaseLog').then((user) => {
+                signup.firstNameInput().type(user.firstName)
+                signup.lastNameInput().type(user.lastName)
+                signup.emailInput().type(user.Email)
+                signup.phonenumberInput().type(user.phonenumber)
+                signup.SelectCountry().type(user.phonenumber)
+                signup.passwordInput().type(user.password)
+                signup.confirmPasswordInput().type(user.confirmPassword)
+                signup.nextButton().should('be.visible').click()
+                cy.contains("To continue، you’ll need to agree to the terms and conditions below.").should('be.visible')
+                signup.acceptButton().should('be.visible').click()
+                cy.contains("Please verify your account in order to complete your regestration").should('be.visible')
+                signup.resendemail().click()
+            })
+            
+            cy.wait('@apiCheck').then((xhr) => {
+                assert.include(xhr.response.body.data, { isValid: true })
+            })
+        })
+    })
+})
