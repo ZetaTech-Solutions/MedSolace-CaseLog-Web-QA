6a95bff129f0023fc571a61f79651595cb93a4c8
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-23 09:05:23 +0200

    Add function in opject page for search process and invalid input

---
+++ cypress/integration/pageObjects/search.js
@@ -0,0 +1,33 @@
+class search{
+ 
+    searchinput(){
+    }
+
+    searchFilterAll(){
+    }
+
+    searchFilterToday(){
+    }
+
+    searchFilterOnwWeek(){
+    }
+
+    searchFilteroneMonth(){
+    }
+
+    selectpatientInfobutton(){
+
+    }
+
+    selectinstitutionbutton(){
+
+    }
+
+    selectcptbutton(){
+        
+    }
+
+    searchbutton(){
+    }
+}
+export default search
