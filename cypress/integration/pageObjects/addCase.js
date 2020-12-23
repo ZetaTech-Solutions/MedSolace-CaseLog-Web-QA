6d540b461676525aa3712b88cc139c7538e06e5b
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-23 12:45:19 +0200

    add require functions and invalid input

---
+++ cypress/integration/pageObjects/addCase.js
@@ -0,0 +1,37 @@
+class AddCase{
+
+    AddCasebutton(){}
+    PatientNameInput(){}
+    GenderInput(){}
+    DateofBirth(){}
+    MedicalRecordNumberInput(){}
+    InsuranceInput(){}
+    DateofSurgery(){}
+    MedicalRecordNumberinput(){}
+    InstituionInput(){}
+    ProcedureTypeInput(){}
+    RoleInput(){}
+    FindingsInput(){}
+    ComplicationsInput(){}
+    OutcomeInput(){}
+    SelectFollowUp(){}
+    SelectDay(){}
+    NoteInput(){}
+    AddNote(){}
+    AddNote2(){}
+    AddImage(){}
+    AddDiagnosis(){}
+    AddDiagnosisSearchInput(){}
+    AddDiagnosisSearchButton(){}
+    AddDiagnosisCheckBox(){}
+    SaveDiagnosis(){}
+    CancelDiagnosis(){}
+    AddCPT(){}
+    AddCPTSearchInput(){}
+    AddCPTSearchButton(){}
+    AddCPTCheckBox(){}
+    SaveAddCPT(){}
+    CancelAddCPT(){}
+    SaveButton(){}
+}
+export default AddCase
