66d9183eb45c71f6b7ac58ef2972b00e3d36d133
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-23 16:04:07 +0200

    [MCL-249] - Verify Change password with valid input

--- cypress/integration/caseLog/Change Password/TC01_changePassword.spec.js
+++ cypress/integration/caseLog/Change Password/TC01_changePassword.spec.js
@@ -1,42 +1,36 @@
-import Login from '../pageObjectsCaseLog/loginCaseLog'
-import ChangePassword from '../pageObjectsCaseLog/changePassword'
+import Login from '../../pageObjects/loginCaseLog'
+import ChangePassword from '../../pageObjects/changePassword'
+import profile from '../../pageObjects/profile'
 
 describe('Change password', function () {
     const login = new Login()
+    const profile = new profile()
     const changePassword= new ChangePassword()
-// Change password
+
     it('Change password', function () {
         cy.server()
         cy.route('POST', '/api/v1/user/Login').as('login')
-        cy.visit('http://s2cript.azurewebsites.net/')
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.visit(url.WebsiteUrl)
+            cy.fixture('userLoginDetailsCaseLog').then((user) => {
+                login.signInButton().should('be.visible').click()
+                login.email().type(user.email)
+                login.password().type(user.password)
+                login.signInButton().eq(1).should('be.visible').click()
+                cy.url().should('include', 'dashboard')
+            })
 
-        cy.fixture('userLoginDetailsCaseLog').then((user) => {
-            login.signInButton().should('be.visible').click()
-            login.email().type(user.email)
-            login.password().type(user.password)
-            login.signInButton().eq(1).should('be.visible').click()
-            cy.url().should('include', 'dashboard')})
-            cy.get('.mat-menu-trigger').should('be.visible').click()
-            cy.get('[data-testid=profile_button]').should('be.visible').click()
-            cy.url().should('include', 'profile')
-            cy.contains("Choose a strong password, Use at least 8 characters with a mix of letters, numbers & symbols. Don’t use a password from another site, or something too obvious like your pet’s name.").should('be.visible')
-
-        cy.wait('@login').its('status').should('eq', 200)
-
-        cy.route('PUT', '/api/v1/user/UpdateUserPassword').as('changepassword')
-      
-        cy.fixture('changePasswordDetailsCaseLog').then((user) => {
-            cy.wait(2000)
-            changePassword.changePasswordInput().click()
-            cy.contains('Change Password').should('be.visible')
-            changePassword.oldPasswordInput().type(user.oldPassword)
-            changePassword.newPasswordInput().type(user.newPassword)
-            changePassword.newPasswordInput().type(user.confirmPassword)
-            changePassword.changePasswordButton().should('be.visible').click()
-            cy.contains('Password changed successfully').should('be.visible')
-            changePassword.okButton().click() 
+            cy.fixture('changePasswordDetailsCaseLog').then((user) => {
+                profile.profileicon().click()
+                profile.editicon().click()
+                profile.changepasswordbutton().click()
+                changePassword.oldPasswordInput().type(user.oldPassword)
+                changePassword.newPasswordInput().type(user.newPassword)
+                changePassword.newPasswordInput().type(user.confirmPassword)
+                changePassword.changePasswordButton().should('be.visible').click()
+                cy.contains(user.Passwordchangedsuccessfully).should('be.visible')
+            })
+            cy.wait('@changepassword').its('status').should('eq', 200)
         })
-        cy.wait('@changepassword').its('status').should('eq', 200)
-       
     })
 })
