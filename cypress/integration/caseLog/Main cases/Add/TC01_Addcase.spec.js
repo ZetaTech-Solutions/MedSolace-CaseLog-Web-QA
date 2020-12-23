583e991c45a15e7b733fa1bc0e8b32b22a1fdbd8
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-23 12:44:43 +0200

    [MCL-221] - Verify Add new case with valid input

--- cypress/integration/caseLog/Main cases/Add/TC01_Addcase.spec.js
+++ cypress/integration/caseLog/Main cases/Add/TC01_Addcase.spec.js
@@ -1,13 +1,12 @@
-import Login from '../pageObjectsCaseLog/loginCaseLog'
-
+import Login from '../../../pageObjects/loginCaseLog'
+import AddCase from '../../../pageObjects/addcase'
 describe('Cases', function () {
     const login = new Login()
     it('cases', function () {
+        cy.fixture('WebsiteUrl').then((url)=>{
         cy.server()
         cy.route('POST', '/api/v1/user/Login').as('login')
-
-        cy.visit('http://')
-
+        cy.visit(url.WebsiteUrl)
         cy.fixture('userLoginDetailsCaseLog').then((user) => {
             login.signInButton().should('be.visible').click()
             login.email().type(user.email)
@@ -17,51 +16,42 @@ describe('Cases', function () {
             cy.wait(1000)
             cy.url().should('include', 'dashboard')
         })
-        cy.get('[data-testid=search_button]').should('be.visible').click()
-        cy.url().should('include', 'search')
 
-        // Add new case
-        cy.fixture('caseDetailsCaseLog').then((user) => {
-            cy("").type(user.patientNameInput)
-            cy("").type(user.genderInput)
-            cy("").type(user.dateOfBirthInput)
-            cy("").type(user.medicalRecordNumberInput)
-            cy("").type(user.insuranceInput)
-            cy("").should('be.visible').click()
-            cy("").type(user.diagnosisInput)
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").type(user.cptInput)
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").type(user.dateOfSurgeryInput)
-            cy("").type(user.institutionInput)
-            cy("").type(user.procedureTypeInput)
-            cy("").type(user.roleInput)
-            cy("").type(user.findingsInput)
-            cy("").type(user.complicationsInput)
-            cy("").type(user.complicationsInput)
-            cy("").type(user.outcomeInput)
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").should('be.visible').click()
-            cy("").type(user.noteInput)
-            cy("").click()
-            cy("").attachFile(image)
-            cy("").click()
-            cy.wait(3000)
-            cy("").should('be.visible').click()
-            cy.get('.alert-container > .mat-focus-indicator').should('be.visible').click()
-            cy("").should('be.visible').click()
+            // Add new case
+            cy.fixture('caseDetailsCaseLog').then((user) => {
+                AddCase.patientNameInput().type(user.patientNameInput)
+                AddCase.genderInput().type(user.genderInput)
+                AddCase.dateOfBirthInput().type(user.dateOfBirthInput)
+                AddCase.medicalRecordNumberInput().type(user.medicalRecordNumberInput)
+                AddCase.institutionInput().type(user.insuranceInput)
 
-            
+                AddCase.AddDiagnosis().should('be.visible').click()
+                AddCase.AddDiagnosisSearchInput().type(user.diagnosisSearchInput)
+                AddCase.AddDiagnosisSearchButton().click()
+                AddCase.AddDiagnosisCheckBox().should('be.visible').click()
+                AddCase.SaveDiagnosis().should('be.visible').click()
+                
+                AddCase.AddCPT().should('be.visible').click()
+                AddCase.AddCPTSearchInput().type(user.cptSearchInput)
+                AddCase.AddCPTSearchButton().click()
+                AddCase.AddCPTCheckBox().should('be.visible').click()
+                AddCase.SaveCPT().should('be.visible').click()
+
+                AddCase.DateOfSurgeryInput().type(user.dateOfSurgeryInput)
+                AddCase.InstitutionInput().type(user.institutionInput)
+                AddCase.ProcedureTypeInput().type(user.procedureTypeInput)
+                AddCase.RoleInput().type(user.roleInput)
+                AddCase.FindingsInput().type(user.findingsInput)
+                AddCase.ComplicationsInput().type(user.complicationsInput)
+                AddCase.OutcomeInput().type(user.outcomeInput)
+                AddCase.SelectFollowUp().should('be.visible')
+                AddCase.SelectDay()
+                AddCase.NoteInput().type(user.noteInput)
+                AddCase.AddNote().click()
+                AddCase.NoteInput2().type(user.noteInput)
+                AddCase.AddImage().attachFile(image)
+                AddCase.SaveButton().click()
+            })
         })
-
     })
 })
