f34059a3ea49b421b001928fb9292db51524b452
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-24 10:34:37 +0200

    Add invalid input and experiance functions

---
+++ cypress/integration/pageObjects/experince.js
@@ -0,0 +1,14 @@
+class Experience{
+
+    ExperienceTab(){}
+    InstitutionInput(){}
+    SpecialityInput(){}
+    AddressInput(){}
+    StartDateInput(){}
+    IcurrentlyworkinthisroleCheckBox(){}
+    Addicon(){}
+    EditIcon(){}
+    SaveButton(){}
+    CancleButton(){}
+}
+export default Experience
