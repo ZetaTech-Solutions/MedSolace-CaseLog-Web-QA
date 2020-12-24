c585d7fda478dd97e26aceafa7d4fcc572d296e7
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-24 09:07:48 +0200

    Add functions for current position test

---
+++ cypress/integration/pageObjects/currentPostion.js
@@ -0,0 +1,13 @@
+class CurrentPostion{
+
+    CurrentPostionTab(){}
+    InstitutionInput(){}
+    SpecialityInput(){}
+    AddressInput(){}
+    StartDateInput(){}
+    IcurrentlyworkinthisroleCheckBox(){}
+    EditIcon(){}
+    SaveButton(){}
+    CancleButton(){}
+}
+export default CurrentPostion
