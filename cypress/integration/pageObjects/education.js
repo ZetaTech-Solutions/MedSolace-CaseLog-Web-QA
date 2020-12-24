373b33f46f570729ace8de930763e9d955d4d0d8
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-24 09:52:24 +0200

    Add education class

---
+++ cypress/integration/pageObjects/education.js
@@ -0,0 +1,14 @@
+class Education{
+
+    EducationTab(){}
+    InstitutionInput(){}
+    SpecialityInput(){}
+    AddressInput(){}
+    StartDateInput(){}
+    IcurrentlyworkinthisroleCheckBox(){}
+    Addicon(){}
+    EditIcon(){}
+    SaveButton(){}
+    CancleButton(){}
+}
+export default Education
