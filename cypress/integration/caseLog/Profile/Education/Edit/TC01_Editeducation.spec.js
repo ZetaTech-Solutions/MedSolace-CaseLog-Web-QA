eb56fbdc6451bf8deeee98ee80364afaa2cba614
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-24 10:22:03 +0200

    [MCL-240] - Verify Edit Education with valid input

---
+++ cypress/integration/caseLog/Profile/Education/Edit/TC01_Editeducation.spec.js
@@ -0,0 +1,41 @@
+import Education from '../../../../pageObjects/education'
+import Login from '../../../../pageObjects/loginCaseLog'
+import Profile from '../../../../pageObjects/profile'
+
+describe('Edit Education', function () {
+    const login = new Login()
+    const education = new Education()
+    const profile = new Profile()
+   
+    it('Profile Information', function () {
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.server()
+            cy.route('POST', '/api/v1/user/Login').as('login')
+            cy.visit(url.WebsiteUrl)
+            cy.fixture('educationDetailsCaseLog').then((user) => {
+                cy.contains("Welcome back!").should('be.visible')
+                login.email().type(user.email)
+                login.password().type(user.password)
+                login.signInButton().click()
+            })
+
+            profile.profileicon().click()
+            cy.url().should('include', 'profile')
+            cy.fixture('currentPositionDetailsCaseLog').then((user) => {
+                education.EducationTab().click()
+                education.EditIcon().click()
+                
+                education.InstitutionInput().clear();
+                education.InstitutionInput().type(user.institutionInput)   
+                education.SpecialityInput().clear();
+                education.SpecialityInput().type(user.specialityInput)
+                education.AddressInput().clear()
+                education.AddressInput().type(user.addressInput)
+                education.StartDateInput().clear()
+                education.StartDateInput().type(user.startDateInput)
+                
+                education.SaveButton()
+            })
+        })
+    })
+})
