d5b741ab8f3a8e7b075b3af95b963caabba3e2bd
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-24 11:39:55 +0200

    [MCL-267] - Verify view any case

---
+++ cypress/integration/caseLog/Main cases/TC01_Viewcase.js
@@ -0,0 +1,22 @@
+import Login from '../../pageObjects/loginCaseLog'
+import Case from '../../pageObjects/addCase'
+
+describe('View case', function () {
+    const login = new Login()
+    const selectcase = new Case()
+   
+    it('Profile Information', function () {
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.server()
+            cy.route('POST', '/api/v1/user/Login').as('login')
+            cy.visit(url.WebsiteUrl)
+            cy.fixture('userLoginDetailsCaseLog').then((user) => {
+                cy.contains("Welcome back!").should('be.visible')
+                login.email().type(user.email)
+                login.password().type(user.password)
+                login.signInButton().click()
+            })
+            selectcase.SelectAnyCase().click()
+        })
+    })
+})
