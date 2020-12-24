690f9926decf4972698dc16da443f25c1ab5506d
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2020-12-24 10:47:16 +0200

    [MCL-248] - Verify Add Experience with invalid input

---
+++ cypress/integration/caseLog/Profile/Experience/Add/TC02_Addexperience_invalid.spec.js
@@ -0,0 +1,38 @@
+import Experience from '../../../../pageObjects/experince'
+import Login from '../../../../pageObjects/loginCaseLog'
+import Profile from '../../../../pageObjects/profile'
+
+describe('Add Experience Invalid', function () {
+    const login = new Login()
+    const experience = new Experience()
+    const profile = new Profile()
+   
+    it('Profile Information', function () {
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.server()
+            cy.route('POST', '/api/v1/user/Login').as('login')
+            cy.visit(url.WebsiteUrl)
+            cy.fixture('educationDetailsCaseLog').then((user) => {
+                cy.contains("Welcome back!").should('be.visible')
+                login.email().type(user.email)
+                login.password().type(user.password)
+                login.signInButton().click()
+            })
+
+            profile.profileicon().click()
+            cy.url().should('include', 'profile')
+            cy.fixture('experiancesDetailsCaseLog').then((user) => {
+                experience.ExperienceTab().click()
+                experience.Addicon().click()
+                
+                experience.InstitutionInput().type(user.institutionInputInvalid)   
+                experience.SpecialityInput().type(user.specialityInputInvalid)
+                experience.AddressInput().type(user.addressInputInvalid)
+                experience.StartDateInput().type(user.startDateInputInvalid)
+                experience.IcurrentlyworkinthisroleCheckBox().click()
+                
+                cy.contains(user.ErrorIndictors);
+            })
+        })
+    })
+})
