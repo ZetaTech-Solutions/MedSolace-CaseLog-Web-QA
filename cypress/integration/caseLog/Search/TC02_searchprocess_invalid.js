3bf62185199fa1ec1f2e4feec7bc3624fb1dbe44
Shuaa Abdoh <shabdoh@zetatechsolutions.com>
2021-01-14 16:23:08 +0200

    Refactor search cypress code and run it

--- cypress/integration/caseLog/Search/TC02_searchprocess_invalid.js
+++ cypress/integration/caseLog/Search/TC02_searchprocess_invalid.js
@@ -1,53 +1,87 @@
 import Login from '../../pageObjects/loginCaseLog'
+import Search from '../../pageObjects/Search'
 
-
-describe('search Filter', function () {
+describe('search Filte invalid', function () {
     const login = new Login()
-        it('search Filter', function () {
-            cy.fixture('WebsiteUrl').then((url)=>{
-            cy.fixture('userLoginDetailsCaseLog').then((user) => {
-                cy.visit(url.WebsiteUrl)
+    const search = new Search()
+    it('patientInfo', function(){
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.server()
+            cy.route('POST', '/api/v1/login').as('login')
+            cy.route('GET','api/v1/users/169/procedures?page=1&page_size=25&filter=ALL&search_by=INFO&value=').as('search')
+            cy.visit(url.WebsiteUrl)
+        
+            cy.fixture('userLoginDetailsCaseLog').then((user)=>{
                 login.email().type(user.email)
                 login.password().type(user.password)
-                login.signInButton().eq(1).should('be.visible').click()
+                login.eyepassword().click()
+                login.signInButton().click()
                 cy.wait('@login').its('status').should('eq', 200)
-
-                cy.wait(100)
-                cy.url().should('include', 'dashboard')
-                cy.get('[data-testid=profile_button]').should('be.visible').click()
-                cy.url().should('include', 'search')
             })
+            cy.url().should('include', 'dashboard') 
+        })
 
-            cy.fixture('searchDetailsCaseLog').then((user) => {
-                //patientInfo
-                cy.searchinput().type(user.patientInfoInputInvalid)
-                cy.searchbutton().click()
-                cy.contains(user.patientInfoInputInvalid).should('be.invisible').click()
-                cy.searchFilterAll()
-                cy.searchFilterToday()
-                cy.searchFilterOnwWeek()
-                cy.searchFilteroneMonth()
-
-                //Institution
-                cy.searchinput().clear()
-                cy.selectpatientInfobutton()
-                cy.searchinput().type(user.institutionInputInvalid)
-                cy.contains(user.institutionInputInvalid).should('be.invisible').click()
-                cy.searchFilterAll()
-                cy.searchFilterToday()
-                cy.searchFilterOnwWeek()
-                cy.searchFilteroneMonth()
-            
-                //cpt
-                cy.searchinput().clear()
-                cy.selectcptbutton()
-                cy.searchinput().type(user.cptInputInvaid)
-                cy.contains(user.cptInputInvaid).should('be.invisible').click()
-                cy.searchFilterAll()
-                cy.searchFilterToday()
-                cy.searchFilterOnwWeek()
-                cy.searchFilteroneMonth()
-            })
+        cy.fixture('searchDetailsCaseLog').then((user) => {
+            search.searchinput().type(user.patientInfoInputInvalid)
+            search.searchbutton().click()
+            search.searchFilterAll().click()
+            search.searchFilterToday().click()
+            search.searchFilterOnwWeek().click()
+            search.searchFilteroneMonth().click()
         })
     })
-})
+
+    it('Institution', function(){
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.server()
+            cy.route('POST', '/api/v1/login').as('login')
+            cy.route('GET','api/v1/users/169/procedures?page=1&page_size=25&filter=ALL&search_by=INFO&value=').as('search')
+            cy.visit(url.WebsiteUrl)
+        
+            cy.fixture('userLoginDetailsCaseLog').then((user)=>{
+                login.email().type(user.email)
+                login.password().type(user.password)
+                login.eyepassword().click()
+                login.signInButton().click()
+                cy.wait('@login').its('status').should('eq', 200)
+            })
+            cy.url().should('include', 'dashboard') 
+        })
+
+        cy.fixture('searchDetailsCaseLog').then((user) => {
+             search.selectpatientInfobutton().click()
+             search.searchinput().type(user.institutionInputInvalid)
+             search.searchFilterAll().click()
+             search.searchFilterToday().click()
+             search.searchFilterOnwWeek().click()
+             search.searchFilteroneMonth().click()
+        })
+    })
+
+    it('cpt', function(){
+        cy.fixture('WebsiteUrl').then((url)=>{
+            cy.server()
+            cy.route('POST', '/api/v1/login').as('login')
+            cy.route('GET','/api/v1/users/169/procedures?page=2&page_size=25&filter=OLD&search_by=CPT&value=Cholera').as('search')
+            cy.visit(url.WebsiteUrl)
+        
+            cy.fixture('userLoginDetailsCaseLog').then((user)=>{
+                login.email().type(user.email)
+                login.password().type(user.password)
+                login.eyepassword().click()
+                login.signInButton().click()
+                cy.wait('@login').its('status').should('eq', 200)
+            })
+            cy.url().should('include', 'dashboard') 
+        })
+
+        cy.fixture('searchDetailsCaseLog').then((user) => {
+            search.selectcptbutton().click()
+            search.searchinput().type(user.cptInputInvaid)
+            search.searchFilterAll().click()
+            search.searchFilterToday().click()
+            search.searchFilterOnwWeek().click()
+            search.searchFilteroneMonth().click()
+        })
+    })
+ })
--- cypress/integration/pageObjects/search.js
+++ cypress/integration/pageObjects/search.js
@@ -1,35 +1,43 @@
-class search{
+class Search{
  
     searchinput(){
+        return cy.get('.MuiInputBase-input')
     }
 
     searchFilterAll(){
+        return cy.get('.MuiPaper-root > .MuiButtonBase-root')
     }
 
     searchFilterToday(){
+        return cy.get('.MuiList-root > :nth-child(2)')
     }
 
     searchFilterOnwWeek(){
+        return cy.get('.MuiList-root > :nth-child(3)')
     }
 
     searchFilteroneMonth(){
+        return cy.get('.MuiList-root > :nth-child(4)')
     }
 
     selectpatientInfobutton(){
-
+        return cy.get('.MuiButtonGroup-root > .MuiButton-contained')
     }
 
     selectinstitutionbutton(){
-
+        return cy.get('.MuiButtonGroup-root > :nth-child(2)')
     }
 
     selectcptbutton(){
-        
+        return cy.get('.MuiButtonGroup-root > :nth-child(3)')
     }
 
     searchbutton(){
+        return cy.get(':nth-child(1) > .jss12 > .MuiBox-root > .MuiButtonBase-root')
     }
 
-    exportbutton(){}
+    exportbutton(){
+        return cy.get(':nth-child(2) > .jss12 > .MuiBox-root > .MuiButtonBase-root')
+    }
 }
-export default search
+export default Search
